759d0fc7c2f9a7ccffbbed2d2ac0a8ae3ee29424
sergpet93 <maknev96@mail.ru>
2023-02-10 12:06:02 +0300

    docs: update index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while(str.length > 0){
+    let checkBracket = false
+    for (let bracket of bracketsConfig){
+      let reg = `${bracket[0]}${bracket[1]}`;
+      if (str.indexOf(reg) !== -1){
+        str = str.replace(reg, '');
+        checkBracket = true
+      }
+    }
+    if (checkBracket === false) break
+  } 
+  return str.length > 0 ? false : true 
 }
